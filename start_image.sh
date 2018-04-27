b22965d50e6b392ebe9a46e626281a35d4b4e9ca
Biomonitor <bio@Biomonitors-MacBook-Pro.local>
2018-04-26 22:57:35 -0400

    more docker stuff

--- start_image.sh
+++ start_image.sh
@@ -2,4 +2,4 @@
 docker build -t biomonitor .
 
 # Run the docker image!
-docker run -it --restart=always -v /dist/zipped:/zipout -v /api/data:/save -v /Volumes/:/Volumes -v ~/Downloads:/Downloads -p 5000:5000 -p 8000:8000 biomonitor
+docker run -it --restart=always -v `pwd`/dist/zipped:/zipout -v `pwd`/api/data:/save -v /Volumes/:/Volumes -v ~/Downloads:/Downloads -p 5000:5000 -p 8000:8000 biomonitor
